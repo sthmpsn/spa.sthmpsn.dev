b0e2d464d7fe42c5df24c1eafdac80bd8af488b8
Steve Thompson <sthmpsn@gmail.com>
2021-12-05 16:45:10 -0600

    configure and define bootstrap 5 overrides and customizations.  Initial site configs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="border-bottom border-secondary text-primary">{{ title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      title: 'Welcome to the new sthmpsn.dev Site'
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'bootstrap';
+import '@/sass/global/styles.scss';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
