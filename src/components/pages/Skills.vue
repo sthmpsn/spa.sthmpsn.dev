9cc6a8cbebca6501c04853d276585ae5fd6161f9
Steve Thompson <sthmpsn@gmail.com>
2021-12-30 16:38:16 -0600

    updated skills card layout and responsiveness

--- src/components/pages/Skills.vue
+++ src/components/pages/Skills.vue
@@ -84,7 +84,7 @@
             </div>
         </div> -->
         <!-- END Skills Showcase -->
-        
+
         <!-- START Skills Chart-->
         <div id="skills-chart" class="row my-4">
             <div id="skill-legend" class="row">
@@ -103,11 +103,10 @@
             </div>
 
             <!-- LEFT OFF HERE -->
-            <div id="skill-list" class="row row-cols-4 g-3 justify-content-center">
-                <div class="col bg-white rounded mx-3 p-3" v-for="skill in skills" :key="skill.name">
-                    <div class="row d-flex align-items-center justify-content-center text-center">
+            <!-- <div id="skill-list" class="row row-cols-4 g-3">
+                <div class="col" v-for="skill in skills" :key="skill.name">
+                    <div class="row text-center">
                         <div class="col-8 title px-3">{{ skill.name}}</div>
-                        <!-- Need to configure the icon background color -->
                         <div
                             class="col-4 icon px-3"
                             :class="skill.level == 'A' ? 'advance'
@@ -117,45 +116,95 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
+            <!-- <div class="container"> -->
+                <div id="skill-list" class="row my-4">
+                    <div 
+                        class="col" 
+                        :class="skill.level == 'A' ? 'advance'
+                                : skill.level == 'I' ? 'intermediate' : 'proficient'"
+                        v-for="skill in skills" :key="skill.name"
+                    >
+                        <div class="card mb-3 p-4 mx-auto" style="width: 300px;">
+                            <div class="row g-0">
+                                <div class="col-md-8 title text-center">
+                                    {{ skill.name }}
+                                </div>
+                                <div class="col-md-4 text-center my-auto">
+                                    <div class="card-body icon p-0">
+                                        {{ skill.level }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!-- </div> -->
         </div>
-
 
         <!-- Turn into component -->
         <div id="education" class="row my-4 p-4">
             <h3>Certs and Degrees</h3>
             <div class="row justify-content-around">
-                <div 
-                    class="certs d-flex align-items-center justify-content-center"
+                <div
+                    class="
+                        certs
+                        d-flex
+                        align-items-center
+                        justify-content-center
+                    "
                     data-bs-toggle="tooltip"
                     title="Master of Business Administration"
                 >
                     <div class="mba-wrapper position-relative">
                         <i class="mba-icon bi bi-mortarboard-fill"></i>
-                        <abbr class="mba-text">MBA</abbr
+                        <abbr class="mba-text">MBA</abbr>
+                        <span class="mba-sub-text d-block small"
+                            >Info Technology</span
                         >
-                        <span class="mba-sub-text d-block small">Info Technology</span>
                     </div>
                 </div>
-                <div class="certs" data-bs-toggle="tooltip" title="University of Minnesota Full Stack Bootcamp">
+                <div
+                    class="certs"
+                    data-bs-toggle="tooltip"
+                    title="University of Minnesota Full Stack Bootcamp"
+                >
                     <img
-                        :src="require(`../../assets/images/certs/uOfM-Fullstack.png`)"
+                        :src="
+                            require(`../../assets/images/certs/uOfM-Fullstack.png`)
+                        "
                         alt="University of Minnesota Full Stack Bootcamp certificate"
                     />
                 </div>
-                <div class="certs d-flex align-items-center" data-bs-toggle="tooltip" title="Comptia Security +">
+                <div
+                    class="certs d-flex align-items-center"
+                    data-bs-toggle="tooltip"
+                    title="Comptia Security +"
+                >
                     <img
-                        :src="require(`../../assets/images/certs/Security+_CE.jpg`)"
+                        :src="
+                            require(`../../assets/images/certs/Security+_CE.jpg`)
+                        "
                         alt="Security Plus Cert"
                     />
                 </div>
-                <div class="certs d-flex align-items-center" data-bs-toggle="tooltip" title="Comptia Network +">
+                <div
+                    class="certs d-flex align-items-center"
+                    data-bs-toggle="tooltip"
+                    title="Comptia Network +"
+                >
                     <img
-                        :src="require(`../../assets/images/certs/Network+_CE.jpg`)"
+                        :src="
+                            require(`../../assets/images/certs/Network+_CE.jpg`)
+                        "
                         alt="Network Plus Cert"
                     />
                 </div>
-                <div class="certs d-flex align-items-center" data-bs-toggle="tooltip" title="Information Technology Infrastructure Library - Foundations version 3">
+                <div
+                    class="certs d-flex align-items-center"
+                    data-bs-toggle="tooltip"
+                    title="Information Technology Infrastructure Library - Foundations version 3"
+                >
                     <img
                         :src="require(`../../assets/images/certs/ITIL-v3.jpg`)"
                         alt="ITIL Cert"
@@ -168,27 +217,28 @@
 </template>
 
 <script>
-import skills from '@/assets/data/skills.json';
-import { Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'; //Bootstrap Tooltips
-
+import skills from "@/assets/data/skills.json";
+import { Tooltip } from "bootstrap/dist/js/bootstrap.esm.min.js"; //Bootstrap Tooltips
 
 export default {
     name: "skills",
     data() {
         return {
-            skills
+            skills,
         };
     },
     mounted() {
         // Enable for all tooltips in this component
-        let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        let tooltipTriggerList = [].slice.call(
+            document.querySelectorAll('[data-bs-toggle="tooltip"]')
+        );
         tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new Tooltip(tooltipTriggerEl)
-        })
-    }
+            return new Tooltip(tooltipTriggerEl);
+        });
+    },
 };
 </script>
 
 <style lang="scss" scoped>
-    @import '../../sass/pages/skills';
+@import "../../sass/pages/skills";
 </style>
