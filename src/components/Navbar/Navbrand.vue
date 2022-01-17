efd207bd24021ddfd0a3ad477d14048b23462d2d
Steve Thompson <sthmpsn@gmail.com>
2022-01-16 22:50:44 -0500

    Turned the Avatar image into an external link to LinkedIn profile

--- src/components/Navbar/Navbrand.vue
+++ src/components/Navbar/Navbrand.vue
@@ -1,6 +1,8 @@
 <template>
     <a class="navbar-brand mx-auto text-lg-center" href="#">
-        <img class="mb-3 rounded-circle d-none d-lg-inline-block" src="../../assets/images/Avatar.svg" alt="Steve Thompson">
+        <a href="https://www.linkedin.com/in/steven-thompson-10a5071b" target="_blank">
+            <img class="mb-3 rounded-circle d-none d-lg-inline-block" src="../../assets/images/Avatar.svg" alt="Steve Thompson">
+        </a>
         <h1 class="fw-light">Steve Thompson</h1>
     </a>
 </template>
